b4b25b63ccc7f63a650b49b0ddc3d2b843c432ad
amyo29 <yawen1999@gmail.com>
2021-03-27 02:22:30 -0500

    custom font working home screen and tab bar, WIP card style custom table cell logic

--- beepboop/Controllers/HomeViewController.swift
+++ beepboop/Controllers/HomeViewController.swift
@@ -12,6 +12,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var alarmTableView: UITableView!
+    @IBOutlet weak var alarmsTabBarItem: UITabBarItem!
     
     private var alarms: [Alarm] = []
     
@@ -23,7 +24,10 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.navigationController?.isNavigationBarHidden = false
-        titleLabel.font = UIFont(name: "JosefinSans-Light", size: 40.0)
+        titleLabel.font = UIFont(name: "JosefinSans-Regular", size: 40.0)
+        let appearance = UITabBarItem.appearance()
+        let attributes = [NSAttributedString.Key.font:UIFont(name: "JosefinSans-Regular", size: 20)]
+        appearance.setTitleTextAttributes(attributes as [NSAttributedString.Key : Any], for: [])
         
         let fontFamilyNames = UIFont.familyNames
             for familyName in fontFamilyNames {
@@ -38,6 +42,10 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         self.view.sendSubviewToBack(self.alarmTableView)
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return alarms.count
     }
@@ -49,8 +57,49 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.alarmNameLabel?.text = self.alarms[row].name
         cell.alarmTimeLabel?.text = self.alarms[row].time
         cell.alarmImageView?.image = UIImage(named: "../Resources/Images/EventPic.png")
+//        let imageName = UIImage(named: transportItems[indexPath.row])
+//        cell.imageView?.image = imageName
         
         return cell
+    }
+    
+//    init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+//        super.init(style: style, reuseIdentifier: reuseIdentifier)
+//
+//        // add shadow on cell
+//        backgroundColor = .clear // very important
+//        layer.masksToBounds = false
+//        layer.shadowOpacity = 0.23
+//        layer.shadowRadius = 4
+//        layer.shadowOffset = CGSize(width: 0, height: 0)
+//        layer.shadowColor = UIColor.blackColor().CGColor
+//
+//        // add corner radius on `contentView`
+//        contentView.backgroundColor = .white
+//        contentView.layer.cornerRadius = 8
+//    }
+    
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        // this will turn on `masksToBounds` just before showing the cell
+        cell.contentView.layer.masksToBounds = true
+        
+        // add shadow on cell
+        cell.backgroundColor = .clear // very important
+        cell.contentView.layer.masksToBounds = false
+        cell.contentView.layer.shadowOpacity = 0.23
+        cell.contentView.layer.shadowRadius = 4
+        cell.contentView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        cell.contentView.layer.shadowColor = UIColor.black.cgColor
+
+            // add corner radius on `contentView`
+        cell.contentView.backgroundColor = .white
+        cell.contentView.layer.cornerRadius = 8
+        
+        // if you do not set `shadowPath` you'll notice laggy scrolling
+        // add this in `willDisplay` method
+        let radius = cell.contentView.layer.cornerRadius
+        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: radius).cgPath
     }
 }
 
